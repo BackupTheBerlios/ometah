0955b81cdb381fa4f0b615a0ccc8794e01dd086d
jpau <jpau>
2005-07-18 08:12:33 +0000

    * added jpgenetic

--- ometah/interface/ometah.cpp
+++ ometah/interface/ometah.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *  $Id: ometah.cpp,v 1.14 2005/07/14 08:03:43 nojhan Exp $
+ *  $Id: ometah.cpp,v 1.15 2005/07/18 08:12:33 jpau Exp $
  *  Copyright : Université Paris 12 Val-de-Marne
  *              (61 avenue du Général de Gaulle, 94010, Créteil, France)
  *  Author : Johann Dréo <nojhan@gmail.com>
@@ -63,8 +63,8 @@ int main(int argc, char ** argv)
   factoryMetaheuristics = new itsSimpleGeneticFactory;
   setMetaheuristic.add( factoryMetaheuristics->create() );
   
-  /*factoryMetaheuristics = new itsJpGeneticFactory;
-  setMetaheuristic.add( factoryMetaheuristics->create() );*/
+  factoryMetaheuristics = new itsJpGeneticFactory;
+  setMetaheuristic.add( factoryMetaheuristics->create() );
   
   
   /*
