b03a6892519d6ab31cf3c2018a79162554f2e3f0
nojhan <nojhan>
2006-05-28 09:12:44 +0000

    * a test interface for the eo binding

---
+++ ometah/metaheuristic/genetic/test_eo.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "SocketProtocol.hpp"
+
+using namespace std;
+
+int main( int argc, char**argv) {
+  cout << CALL << endl << DEFAULT_PORT << endl << MAXHOSTNAME << endl;
+  return 0;
+}
